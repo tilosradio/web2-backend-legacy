2c8e41a4c5f91b7b0e0a02d0ddc5d45afa0f8df6
Elek, Marton <level2@anzix.net>
2013-10-11 07:12:54 +0200

    show and author and program pages with content from the REST backend

--- yeoman/app/scripts/app.js
+++ yeoman/app/scripts/app.js
@@ -1,25 +1,43 @@
 'use strict';
 
 var tilos = angular.module('tilos', ['ngRoute']);
-     
+
 tilos.config(['$routeProvider', function($routeProvider) {
-   $routeProvider.when('/index', {
-      templateUrl: 'partials/index.html',
-      controller: 'IndexCtrl'
-   }).when('/program', {
-      templateUrl: 'partials/program.html',
-      controller:  'ProgramCtrl'
-   }).otherwise({
-      redirectTo: '/index'
-   });
-}]);
+        $routeProvider.when('/index', {
+            templateUrl: 'partials/index.html',
+            controller: 'IndexCtrl'
+        }).when('/program', {
+            templateUrl: 'partials/program.html',
+            controller: 'ProgramCtrl'
+        }).when('/show/:id', {
+            templateUrl: 'partials/show.html',
+            controller: 'ShowCtrl'
+        }).when('/author/:id', {
+            templateUrl: 'partials/author.html',
+            controller: 'AuthorCtrl'
+        }).otherwise({
+            redirectTo: '/index'
+        });
+    }]);
 
 tilos.controller('IndexCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-   $scope.test = "test";
-}]);
-var res;
+        $scope.test = "test";
+    }]);
+
+tilos.controller('AuthorCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+        $http.get('http://localhost/api/author/' + $routeParams.id).success(function(data) {
+            $scope.author = data;
+        });
+    }]);
+
+tilos.controller('ShowCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+        $http.get('http://localhost/api/show/' + $routeParams.id).success(function(data) {
+            $scope.show = data;
+        });
+    }]);
+
 tilos.controller('ProgramCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
-   $http.get('episode.json').success(function(data) {
-       $scope.episodes = data;
-   });
-}]);
+        $http.get('http://localhost/api/episode').success(function(data) {
+            $scope.episodes = data;
+        });
+    }]);
