706eb7f9633cf720aed77098f80e872ae998685a
rudywaltz <david.barkoczi@gmail.com>
2013-10-15 14:21:47 +0200

    init bootsrap.js
    change whit right author

--- yeoman/app/scripts/main.js
+++ yeoman/app/scripts/main.js
@@ -54,7 +54,7 @@ require.config({
     }
 });
 
-require(['app', 'jquery'], function (app, $) {
+require(['app', 'jquery','bootstrapTooltip', 'bootstrapCollapse' ], function (app, $) {
     'use strict';
     // use app here
     console.log(app);
