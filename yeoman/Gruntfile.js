979b4663883aeef86cb3b7e2cb9ce87f7fe73139
David Barkoczi <david.barkoczi@gmail.com>
2013-10-19 20:40:15 +0200

    change icon fonts
    iconmoon update

--- yeoman/Gruntfile.js
+++ yeoman/Gruntfile.js
@@ -34,6 +34,7 @@ module.exports = function (grunt) {
                 },
                 files: [
                     '<%= yeoman.app %>/*.html',
+                    '<%= yeoman.app %>/partials/*.html',
                     '.tmp/styles/{,*/}*.css',
                     '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
                     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
