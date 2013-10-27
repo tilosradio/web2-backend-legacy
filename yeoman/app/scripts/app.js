c2ec2425091b2b59f125fa8fa93a048162801cd6
Elek, Marton <level2@anzix.net>
2013-10-28 00:35:18 +0100

    separated talk and sound type shows

--- yeoman/app/scripts/app.js
+++ yeoman/app/scripts/app.js
@@ -78,7 +78,17 @@ tilos.controller('PageCtrl', ['$scope', '$routeParams', 'tilosData', function($s
 
 tilos.controller('AllShowCtrl', ['$scope', '$routeParams', 'API_SERVER_ENDPOINT', '$http', function($scope, $routeParams, $server, $http) {
         $http.get($server + '/api/show').success(function(data) {
-            $scope.shows = data;            
+            var res = {'talk':[],'sound':[]}
+            for (var i = 0;i<data.length;i++){
+               var show = data[i];
+               if (show.type) {
+                   res.talk.push(data[i]);
+               } else {
+                   res.sound.push(data[i]);
+               }
+            }
+            $scope.shows = res;
+            
         });
     }]);
 
