f1d451197aea58a9f19dff020a7cbe1ebfb767df
David Barkoczi <david.barkoczi@gmail.com>
2013-10-28 19:55:03 +0100

    minifile fix

--- yeoman/Gruntfile.js
+++ yeoman/Gruntfile.js
@@ -314,8 +314,8 @@ module.exports = function (grunt) {
             dist: [
                 'compass',
                 'copy:styles',
-                'imagemin',
-                'svgmin',
+                // 'imagemin',
+                // 'svgmin',
                 'htmlmin'
             ]
         },
@@ -361,12 +361,28 @@ module.exports = function (grunt) {
         'concat',
         'cssmin',
         'uglify',
-// TODO fix the run of this tool
-//        'modernizr',
-	'copy:php',
+        'copy:php',
         'copy:dist',
         'rev',
-        'usemin'
+        'usemin',
+         'connect:livereload',
+    ]);
+    grunt.registerTask('buildlive', [
+        'clean:dist',
+        'useminPrepare',
+        'concurrent:dist',
+        'autoprefixer',
+//TODO fix the parameters
+        'requirejs',
+        'concat',
+        'cssmin',
+        'uglify',
+	    'copy:php',
+        'copy:dist',
+        'rev',
+        'usemin',
+        'connect:livereload',
+        'watch'
     ]);
 
     grunt.registerTask('default', [
