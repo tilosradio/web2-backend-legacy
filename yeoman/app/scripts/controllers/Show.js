094e5c076ea325f606e0bc81a64bb63fc987f133
Eva Hajdu <vid.eskin@gmail.com>
2013-12-04 16:46:29 +0000

    Kisebb valtoztatasok (elcsuszott divek stb.)

--- yeoman/app/scripts/controllers/Show.js
+++ yeoman/app/scripts/controllers/Show.js
@@ -8,7 +8,7 @@ angular.module('tilosApp')
 
 			$scope.show.sharecount = 0;
 
-			$scope.likeURL = validateUrl.getValidUrl('http://www.facebook.com/plugins/like.php?href=http%3A%2F%2F'+server+'%2F%23%2Fshow%2F'+$scope.show.alias+'&width&layout=standard&action=like&show_faces=true&share=true');
+			$scope.likeURL = validateUrl.getValidUrl('http://www.facebook.com/plugins/like.php?href=http%3A%2F%2F'+tilosHost+'%2F%23%2Fshow%2F'+$scope.show.alias+'&width&layout=standard&action=like&show_faces=true&share=true');
 
 			$http.get("https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27http%3A%2F%2F"+server+"%2F%23%2Fshow%2F"+$scope.show.alias+"%27").success(function (data) {
 				if(data.data[0] != undefined){
