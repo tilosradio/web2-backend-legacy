94453f83b87507d69f2f9d9eea8626e57c5d8642
David Barkoczi <david.barkoczi@gmail.com>
2013-12-08 21:49:20 +0100

    design fix
    Smaller social icon,  prevent long link in sidebar. change rss color

--- yeoman/app/scripts/directives/activeLink.js
+++ yeoman/app/scripts/directives/activeLink.js
@@ -6,14 +6,12 @@ angular.module('tilosApp')
     restrict: 'A',
     link: function (scope, element, attrs) {
       var clazz = attrs.activeLink;
-
       //TODO it shoud be more error prone
       var path = element.children()[0].href;
       path = path.substring(1 + path.indexOf('#'));
       if (path.charAt(0) !== '/') {
         path = '/' + path;
       }
-
       scope.location = location;
       scope.$watch('location.path()', function (newPath) {
         dbg = element;
