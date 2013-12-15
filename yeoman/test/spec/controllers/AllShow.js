e1ba450dad76c21690a276df07685780a2da7ddc
Elek, Márton <level2@anzix.net>
2013-12-15 18:22:17 +0100

    first working javascript unit test

--- yeoman/test/spec/controllers/AllShow.js
+++ yeoman/test/spec/controllers/AllShow.js
@@ -1,22 +1,39 @@
 'use strict';
+(function () {
+  describe('Controller: AllshowCtrl', function () {
 
-describe('Controller: AllshowCtrl', function () {
+    // load the controller's module
+    beforeEach(module('tilosApp'));
 
-  // load the controller's module
-  beforeEach(module('tilosApp'));
+    var createController;
+    var scope;
+    var $httpBackend;
 
-  var AllshowCtrl,
-    scope;
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($injector, $rootScope) {
+      scope = $rootScope.$new();
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    AllshowCtrl = $controller('AllshowCtrl', {
-      $scope: scope
+      var $controller = $injector.get('$controller');
+
+      createController = function () {
+        return $controller('AllshowCtrl', {
+            '$scope': $rootScope,
+            'API_SERVER_ENDPOINT': "http://server"
+          }
+        );
+      };
+      $httpBackend = $injector.get('$httpBackend');
+    }));
+
+    it('show classify returned shows', function () {
+      $httpBackend.expectGET('http://server/api/v0/show').respond([
+        {id: 1, type: 1},
+        {id: 2, type: 0}
+      ]);
+      var controller = createController();
+      $httpBackend.flush();
+      chai.expect(scope.shows.talk.length).to.equal(1);
+      chai.expect(scope.shows.sound.length).to.equal(1);
     });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
   });
-});
+})();
