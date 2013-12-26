de582f5977829f56465386b441ca92dd634c6132
Elek, Márton <level2@anzix.net>
2013-12-26 19:20:05 +0100

    invalidate cache after edited author

--- yeoman/app/scripts/controllers/AuthorEdit.js
+++ yeoman/app/scripts/controllers/AuthorEdit.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tilosApp')
-  .controller('AuthorEditCtrl', ['$scope', '$routeParams', 'API_SERVER_ENDPOINT', '$http', function ($scope, $routeParams, server, $http) {
+  .controller('AuthorEditCtrl', ['$location', '$scope', '$routeParams', 'API_SERVER_ENDPOINT', '$http', '$cacheFactory', function ($location, $scope, $routeParams, server, $http, $cacheFactory) {
     if ($routeParams.id) {
       $http.get(server + '/api/v0/author/' + $routeParams.id, {cache: true}).success(function (data) {
         $scope.author = data;
@@ -12,7 +12,9 @@ angular.module('tilosApp')
     $scope.save = function () {
       if ($scope.author.id) {
         $http.put(server + '/api/v0/author/' + $routeParams.id, $scope.author).success(function (data) {
-          $scope.author = data;
+          var httpCache = $cacheFactory.get('$http');
+          httpCache.remove(server + '/api/v0/author/' + $scope.author.id);
+          $location.path('/author/' + $scope.author.id);
         });
       } else {
 
--- yeoman/app/scripts/controllers/Login.js
+++ yeoman/app/scripts/controllers/Login.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('tilosApp')
-  .controller('LoginCtrl', ['$scope', '$location', 'API_SERVER_ENDPOINT', '$http', function ($scope, $location, server, $http) {
+  .controller('LoginCtrl', ['$rootScope', '$scope', '$location', 'API_SERVER_ENDPOINT', '$http', function ($rootScope, $scope, $location, server, $http) {
     $scope.logindata = [];
     $scope.login = function () {
       var data = {};
-      data['username'] = 'admin';
-      data.password = 'qweqweqweqwe';
+      data['username'] = $scope.logindata.username;
+      data.password = $scope.logindata.password;
       $http.post(server + '/api/v0/auth/sign_in', data).success(function (data) {
         if (data.success) {
+          $rootScope.user = data;
           $location.path('/index');
         }
 
