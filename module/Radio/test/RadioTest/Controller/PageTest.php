5b5ab20f627a87ba3a0dcdeee412d63481dda0c4
Elek, Márton <level2@anzix.net>
2013-12-26 21:46:41 +0100

    fixing test

--- module/Radio/test/RadioTest/Controller/PageTest.php
+++ module/Radio/test/RadioTest/Controller/PageTest.php
@@ -40,7 +40,7 @@ class PageTest extends TestBase {
 
         $page = $result->getVariables();
         //var_dump($page);
-        $this->assertContains("Szabadrádió", $page['content']);
+        $this->assertContains("Szabad Rádió", $page['content']);
         
     }
 
