05470ead06c6a55774c60bc6b2b30d5777d1932b
Elek, Márton <level2@anzix.net>
2013-12-31 17:15:08 +0100

    enabling cache for program list page

--- yeoman/app/scripts/controllers/AllShow.js
+++ yeoman/app/scripts/controllers/AllShow.js
@@ -2,19 +2,19 @@
 
 angular.module('tilosApp')
   .controller('AllshowCtrl', ['$scope', '$routeParams', 'API_SERVER_ENDPOINT', '$http', function ($scope, $routeParams, $server, $http) {
-  $http.get($server + '/api/v0/show').success(function (data) {
-    var res = {
-      talk: [],
-      sound: []
-    };
-    for (var i = 0; i < data.length; i++) {
-      var show = data[i];
-      if (show.type) {
-        res.talk.push(data[i]);
-      } else {
-        res.sound.push(data[i]);
+    $http.get($server + '/api/v0/show', {cache: true}).success(function (data) {
+      var res = {
+        talk: [],
+        sound: []
+      };
+      for (var i = 0; i < data.length; i++) {
+        var show = data[i];
+        if (show.type) {
+          res.talk.push(data[i]);
+        } else {
+          res.sound.push(data[i]);
+        }
       }
-    }
-    $scope.shows = res;
-  });
-}]);
+      $scope.shows = res;
+    });
+  }]);
