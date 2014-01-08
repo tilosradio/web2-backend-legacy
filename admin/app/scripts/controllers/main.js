fce34201bbc58f0b2c90eb427ee5d2f2acabd41f
Elek, Márton <level2@anzix.net>
2014-01-08 22:18:54 +0100

    scaffolding new angular appilcation to the admin subdirectory

---
+++ admin/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('tilosAdminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
