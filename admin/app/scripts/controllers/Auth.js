4bb3f7434cb7409449c37cd731ebfaed9f0588ff
Elek, Márton <level2@anzix.net>
2014-01-11 21:49:07 +0100

    design and content improvements

--- admin/app/scripts/controllers/Auth.js
+++ admin/app/scripts/controllers/Auth.js
@@ -1,12 +1,10 @@
 'use strict';
 
 angular.module('tilosAdmin').controller('AuthCtrl', function ($scope, $routeParams, API_SERVER_ENDPOINT, $http, $location) {
-
-
   $scope.logout = function () {
     $http.get(API_SERVER_ENDPOINT + '/api/v0/auth/sign_out').success(function (data) {
       $scope.user = null;
-      $location.path = '/login';
+      $location.path('/login');
     });
   };
 });
