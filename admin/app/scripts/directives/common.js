b8cca9824d85c39269c0b336878a5fd947a1376c
Elek, Márton <level2@anzix.net>
2014-01-28 22:03:51 +0100

    confirmation dialog for delete oprations. Closes #77

--- admin/app/scripts/directives/common.js
+++ admin/app/scripts/directives/common.js
@@ -23,3 +23,24 @@ angular.module('tilosAdmin')
         }
       };
     }]);
+
+
+angular.module('tilosAdmin').directive('ngConfirmClick', [
+  function () {
+    return {
+      priority: 100,
+      restrict: 'A',
+      link: {
+        pre: function (scope, element, attrs) {
+          element.bind('click touchstart', function (e) {
+            var message = "Nyomj az OK-ra, ha tudod mit csinalsz.";
+            if (message && !window.confirm(message)) {
+              e.stopImmediatePropagation();
+              e.preventDefault();
+            }
+          });
+        }
+      }
+    }
+  }
+]);
