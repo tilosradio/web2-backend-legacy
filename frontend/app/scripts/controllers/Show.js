9bae0e6bda7398a3db70776738d69df4cc01a8d7
Elek, Márton <level2@anzix.net>
2014-03-19 23:05:26 +0100

    fixing fb share url

--- frontend/app/scripts/controllers/Show.js
+++ frontend/app/scripts/controllers/Show.js
@@ -9,7 +9,7 @@ angular.module('tilosApp')
 
       $scope.show.sharecount = 0;
 
-      $scope.likeURL = validateUrl.getValidUrl('http://www.facebook.com/plugins/like.php?href=http%3A%2F%2F' + tilosHost + '%2F%23%2Fshow%2F' + $scope.show.alias + '&width&layout=standard&action=like&show_faces=true&share=true');
+      $scope.likeURL = validateUrl.getValidUrl('http://www.facebook.com/plugins/like.php?href=http%3A%2F%2F' + tilosHost + '%2Fshow%2F' + $scope.show.alias + '&width&layout=standard&action=like&show_faces=true&share=true');
 
       $scope.currentShowPage = 0;
 
@@ -31,7 +31,7 @@ angular.module('tilosApp')
         });
       };
 
-      $http.get('https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27http%3A%2F%2F' + API_SERVER_ENDPOINT + "%2F%23%2Fshow%2F" + $scope.show.alias + "%27", {cache: true}).success(function (data) {
+      $http.get('https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27http%3A%2F%2F' + API_SERVER_ENDPOINT + "2Fshow%2F" + $scope.show.alias + "%27", {cache: true}).success(function (data) {
         if (data.data[0] !== undefined) {
           $scope.show.sharecount = data.data[0].share_count;
         }
