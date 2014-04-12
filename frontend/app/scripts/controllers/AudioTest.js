63f8b147097a9f369cac2ecdc47b294aed86ba2b
Elek, Márton <level2@anzix.net>
2014-04-12 13:18:18 +0200

    jplayer test. Check /audiotest page.

---
+++ frontend/app/scripts/controllers/AudioTest.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('tilosApp').config(function ($routeProvider) {
+    $routeProvider.when('/audiotest', {
+        templateUrl: 'partials/audiotest.html',
+        controller: 'AudioTestCtrl',
+    });
+});
+;
+
+angular.module('tilosApp').controller('AudioTestCtrl', ['$scope', function ($scope) {
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function () {
+            $(this).jPlayer("setMedia", {
+                title: "Tilos",
+                mp3: "http://archive.tilos.hu/online/2014/04/03/tilosradio-20140403-1000.mp3"
+            });
+        },
+        swfPath: "/js",
+        supplied: "mp3"
+    });
+}]);
