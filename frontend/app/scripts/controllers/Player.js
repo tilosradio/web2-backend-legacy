e597ed91c2662239a2772b5131636ef106e4aecb
Elek, Márton <level2@anzix.net>
2014-04-23 23:29:55 +0200

    play the first mp3 of the episode on clock

--- frontend/app/scripts/controllers/Player.js
+++ frontend/app/scripts/controllers/Player.js
@@ -2,17 +2,24 @@
 
 
 angular.module('tilosApp').factory("Player", function ($rootScope) {
+    var jplayer = $("#jquery_jplayer_1");
     $("#jquery_jplayer_1").jPlayer({
         ready: function () {
 
         },
-        swfPath: "/js",
-        supplied: "mp3"
+        swfPath: "/jplayer",
+        nativeSupport: false,
+        solution: "flash, html"
     });
     var player = {};
+
     player.play = function(url) {
-        alert(url);
+        jplayer.jPlayer("setMedia", {
+            title: "Tilos",
+            mp3: url[0].url
+        });
         $rootScope.player = true;
+        jplayer.jPlayer("play");
     };
     return player;
 
