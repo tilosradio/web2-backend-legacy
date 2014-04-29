43f2f2ec64f0f607804fafa92b5752b88c7d20e7
Elek, Márton <level2@anzix.net>
2014-04-29 23:08:38 +0200

    fixing initialization problem

--- frontend/app/scripts/controllers/Player.js
+++ frontend/app/scripts/controllers/Player.js
@@ -3,25 +3,40 @@
 
 angular.module('tilosApp').factory("Player", function ($rootScope) {
     var jplayer = $("#jquery_jplayer_1");
+    var ready = false;
+    var todo = null;
     $("#jquery_jplayer_1").jPlayer({
         ready: function () {
-
+	    ready = true;
+            if (todo != null) {
+		$(this).jPlayer("setMedia", {
+		    title: "Tilos",
+		    mp3: todo
+		});
+		$(this).jPlayer('play');
+		todo = null;
+	    }
         },
         swfPath: "/jplayer/Jplayer.swf",
         nativeSupport: false,
         solution: "flash, html",
         errorAlerts: true,
-        warningAlerts: true
+        warningAlerts: true,
     });
     var player = {};
 
     player.play = function(url) {
-        jplayer.jPlayer("setMedia", {
-            title: "Tilos",
-            mp3: url[0].url
-        });
+	if (ready) {
+            jplayer.jPlayer("setMedia", {
+		title: "Tilos",
+		mp3: url[0].url
+            });
+	    jplayer.jPlayer('play');
+	} else {
+	    todo = url[0].url;
+	}
         $rootScope.player = true;
-        jplayer.jPlayer("play");
+
     };
     return player;
 
