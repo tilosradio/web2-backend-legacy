43f792773683b90fcedb15aba6a8361b57f8fc1f
Elek, Márton <level2@anzix.net>
2014-07-06 13:54:01 +0200

    enable unit tests again with dbunit test data

--- util/service/src/test/java/hu/tilos/radio/backend/SearchControllerTest.java
+++ util/service/src/test/java/hu/tilos/radio/backend/SearchControllerTest.java
@@ -4,7 +4,11 @@ import hu.tilos.radio.backend.data.SearchResponse;
 import hu.tilos.radio.backend.data.SearchResponseElement;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.openjpa.persistence.jdbc.Index;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlProducer;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -15,8 +19,14 @@ import static org.junit.Assert.*;
 
 public class SearchControllerTest {
 
+    @BeforeClass
+    public static void testDataInit() throws Exception {
+        JdbcDatabaseTester tester = new JdbcDatabaseTester("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/tilos_test", "root", "");
+        tester.setDataSet(new FlatXmlDataSet(SearchControllerTest.class.getResourceAsStream("baseData.xml")));
+        tester.onSetup();
+    }
+
     @Test
-    @Ignore
     public void test() throws IOException, ParseException {
         //given
         SearchController controller = new SearchController();
