4a1a17c14989f437c9f4cb882c7ea294ab899b41
Elek, Márton <level2@anzix.net>
2014-08-22 22:48:16 +0200

    Make Mix Category descriptive

--- util/model/src/main/java/hu/radio/tilos/model/type/MixCategory.java
+++ util/model/src/main/java/hu/radio/tilos/model/type/MixCategory.java
@@ -1,9 +1,23 @@
 package hu.radio.tilos.model.type;
 
-public enum MixCategory {
-    DJ,
-    GUEST_DJ,
-    SHOW,
-    PARTY,
-    TALE
+public enum MixCategory implements DescriptiveType {
+    DJ("Tilos DJ"),
+    GUEST_DJ("Vendég DJ"),
+    SHOW("Beszélgetős műsor"),
+    PARTY("Tilos party"),
+    TALE("Tilos mese");
+
+    private String description;
+
+    MixCategory(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
