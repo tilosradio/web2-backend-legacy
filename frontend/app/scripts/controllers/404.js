2cff3d5545ce483ef75a84dce95b2ad75aa46279
Elek, Márton <level2@anzix.net>
2014-08-28 21:24:31 +0200

    supporting very short urls

--- frontend/app/scripts/controllers/404.js
+++ frontend/app/scripts/controllers/404.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('tilosApp')
-  .controller('404Ctrl', function () {
-
-});
+    .controller('404Ctrl', function ($http, API_SERVER_ENDPOINT, $location) {
+        var name = $location.path();
+        if (name[0] === '/') {
+            name = name.substr(1);
+        }
+        $http.get(API_SERVER_ENDPOINT + '/api/v0/text/' + name).success(function (data) {
+            $location.path('/page/' + name);
+        }).error(function (data) {
+            $http.get(API_SERVER_ENDPOINT + '/api/v0/show/' + name).success(function (data) {
+                $location.path('/show/' + name);
+            }).error(function (data) {
+                $http.get(API_SERVER_ENDPOINT + '/api/v0/author/' + name).success(function (data) {
+                    $location.path('/author/' + name);
+                });
+            });
+        });
+    });
