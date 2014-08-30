c6613b92f6fcec2c74ae46b1e8d67f4527d40b95
Elek, Márton <level2@anzix.net>
2014-08-30 19:01:17 +0200

    upgrader class for the migrator

--- util/model/src/main/java/hu/radio/tilos/model/tools/Upgrader.java
+++ util/model/src/main/java/hu/radio/tilos/model/tools/Upgrader.java
@@ -1,4 +1,18 @@
 package hu.radio.tilos.model.tools;
 
+import org.flywaydb.core.Flyway;
+
 public class Upgrader {
+
+    public static void main(String[] args) {
+        new Upgrader().upgrade();
+    }
+
+    private void upgrade() {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource("jdbc:mysql://localhost:3306/tilos2", "root", "");
+        flyway.repair();
+        flyway.migrate();
+        //flyway.init();
+    }
 }
