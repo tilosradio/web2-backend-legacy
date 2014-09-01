b7389f59f709bf8e3f2f88631265623acbdc4d23
Elek, Márton <level2@anzix.net>
2014-09-01 23:08:16 +0200

    build script fixes

--- build_jbackend.sh
+++ build_jbackend.sh
@@ -1,7 +1,8 @@
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/build
 cd $DIR/util
-mvn clean install
+mvn clean install -DskipTests
 cp $DIR/util/service/target/service.war $DIR/build/
 cp $DIR/util/streamer/target/streamer.war $DIR/build/
 cd $DIR
