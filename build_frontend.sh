f82e7903731a0e2dd91351ace3bd983af86d22ba
Elek, Márton <level2@anzix.net>
2014-09-01 21:42:43 +0200

    frontend build script

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,8 +1,9 @@
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/build
 cd $DIR/frontend
 gulp build
-cd dist
+cd $DIR/frontend/dist
 rm $DIR/frontend.zip
 zip -r $DIR/build/frontend.zip *
 cd $DIR
