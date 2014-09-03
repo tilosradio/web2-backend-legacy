aeaf29f3dd3033619d0e925d1ed3b6f5410b66b1
Elek, Márton <level2@anzix.net>
2014-09-03 23:09:43 +0200

    build script updates

--- build_admin.sh
+++ build_admin.sh
@@ -1,6 +1,8 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/build
+cd $DIR/backend
+php composer.phar update
 cd $DIR/admin
 bower install
 gulp build
--- build_frontend.sh
+++ build_frontend.sh
@@ -1,6 +1,8 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/build
+cd $DIR/backend
+php composer.phar update
 cd $DIR/frontend
 gulp build
 cd $DIR/frontend/dist
