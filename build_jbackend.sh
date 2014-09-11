2a186525ebb9d24403c284632248af9016789844
Elek, Márton <level2@anzix.net>
2014-09-11 22:10:24 +0200

    enable unit tests

--- build_jbackend.sh
+++ build_jbackend.sh
@@ -2,7 +2,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/build
 cd $DIR/util
-mvn clean install -DskipTests
+mvn clean install
 cp $DIR/util/service/target/service.war $DIR/build/
 cp $DIR/util/streamer/target/streamer.war $DIR/build/
 cd $DIR
