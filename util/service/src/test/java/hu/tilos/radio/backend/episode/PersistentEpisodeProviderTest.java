ddde19ce6e1387b9d171d336bcd2a8d0eda9b7fa
Elek, Márton <level2@anzix.net>
2014-09-27 20:09:03 +0200

    unit test fix

--- util/service/src/test/java/hu/tilos/radio/backend/episode/PersistentEpisodeProviderTest.java
+++ util/service/src/test/java/hu/tilos/radio/backend/episode/PersistentEpisodeProviderTest.java
@@ -2,34 +2,39 @@ package hu.tilos.radio.backend.episode;
 
 import ch.qos.logback.classic.Level;
 import hu.tilos.radio.backend.TestUtil;
+import hu.tilos.radio.backend.converters.MappingFactory;
 import hu.tilos.radio.backend.data.types.EpisodeData;
+import org.jglue.cdiunit.AdditionalClasses;
+import org.jglue.cdiunit.CdiRunner;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import javax.sql.DataSource;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+@RunWith(CdiRunner.class)
+@AdditionalClasses({MappingFactory.class, TestUtil.class})
 public class PersistentEpisodeProviderTest {
 
     private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    private static DataSource ds;
+    @Inject
+    PersistentEpisodeProvider p;
 
     @BeforeClass
     public static void setUp() {
-        ds = TestUtil.initDatasource();
         TestUtil.initTestData();
     }
 
     @Test
     public void testListEpisode() throws Exception {
         //given
-        PersistentEpisodeProvider p = new PersistentEpisodeProvider();
-        p.setEntityManager(TestUtil.initPersistence().createEntityManager());
 
 
         //when
@@ -38,10 +43,10 @@ public class PersistentEpisodeProviderTest {
         //then
         Assert.assertEquals(2, episodes.size());
         Assert.assertNotNull(episodes.get(0).getShow());
- //       Assert.assertNotNull(episodes.get(0).getText());
-//        Assert.assertEquals("Jo kis beszelgetes 1", episodes.get(0).getText().getTitle());
+//      Assert.assertNotNull(episodes.get(0).getText());
+//      Assert.assertEquals("Jo kis beszelgetes 1", episodes.get(0).getText().getTitle());
         Assert.assertNotNull(episodes.get(1).getText());
-        Assert.assertEquals("Jo musor",episodes.get(1).getText().getTitle());
+        Assert.assertEquals("Jo musor", episodes.get(1).getText().getTitle());
 
 
     }
