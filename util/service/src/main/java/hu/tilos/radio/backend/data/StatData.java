507130fdcab9dfe6264678995ad856d3809f24ed
Elek, Márton <level2@anzix.net>
2014-10-05 15:10:58 +0200

    statistical rest endpoint

---
+++ util/service/src/main/java/hu/tilos/radio/backend/data/StatData.java
@@ -0,0 +1,12 @@
+package hu.tilos.radio.backend.data;
+
+public class StatData {
+
+    public long showCount;
+
+    public long mixCount;
+
+    public long episodeCount;
+
+    public long authorCount;
+}
