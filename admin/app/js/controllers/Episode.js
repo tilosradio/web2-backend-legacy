8e934ca50b39e3e3ee87ec3dd2d8cfa2d7debf57
Elek, Márton <level2@anzix.net>
2014-10-11 22:46:51 +0200

    fixing epch scale for episode edit

--- admin/app/js/controllers/Episode.js
+++ admin/app/js/controllers/Episode.js
@@ -32,20 +32,20 @@ angular.module('tilosAdmin').factory('dateUtil', function () {
     return {
         toHourMin: function (epoch) {
             var d = new Date();
-            d.setTime(epoch * 1000);
+            d.setTime(epoch);
             var result = "" + (d.getHours() < 10 ? "0" : "" ) + d.getHours() + ':' + (d.getMinutes() < 10 ? "0" : "") + d.getMinutes() + ":" + (d.getSeconds() < 10 ? "0" : "") + d.getSeconds();
             return result;
         },
         setDate: function (dateEpoch, dateStr) {
             var date = new Date();
-            date.setTime(dateEpoch * 1000);
+            date.setTime(dateEpoch);
             var parts = dateStr.split(':');
             date.setHours(parseInt(parts[0], 10));
             date.setMinutes(parseInt(parts[1], 10));
             if (parts.length > 2) {
                 date.setSeconds(parseInt(parts[2], 10));
             }
-            return date.getTime() / 1000;
+            return date.getTime();
         }
     };
 })
