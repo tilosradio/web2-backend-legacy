cae47277419137c981bf16254518fdc1d0afea91
Elek, Márton <level2@anzix.net>
2014-10-12 09:42:45 +0200

    fix(migration): nullable textcontent content

--- util/model/src/main/resources/db/migration/V1_6__FieldFixes.sql
+++ util/model/src/main/resources/db/migration/V1_6__FieldFixes.sql
@@ -6,9 +6,7 @@ alter table textcontent modify column created datetime default CURRENT_TIMESTAMP
 
 alter table textcontent modify column modified datetime default CURRENT_TIMESTAMP;
 
-alter table textcontent modify column content longtext default '';
-
-alter table textcontent modify column content longtext default '';
+alter table textcontent modify column content longtext null;
 
 alter table textcontent modify column alias varchar(60) default '';
 
