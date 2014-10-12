063f725b709eede7164e3fab0557cf5d666caf03
Elek, Márton <level2@anzix.net>
2014-10-12 08:58:15 +0200

    fix default values of sql column definitions

---
+++ util/model/src/main/resources/db/migration/V1_6__FieldFixes.sql
@@ -0,0 +1,14 @@
+alter table mix modify column content longtext null;
+
+alter table textcontent modify column author varchar(255) null default '';
+
+alter table textcontent modify column created datetime default CURRENT_TIMESTAMP;
+
+alter table textcontent modify column modified datetime default CURRENT_TIMESTAMP;
+
+alter table textcontent modify column content longtext default '';
+
+alter table textcontent modify column content longtext default '';
+
+alter table textcontent modify column alias varchar(60) default '';
+
